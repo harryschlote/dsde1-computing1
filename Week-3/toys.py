4dea4d4287b6c54001e8e925ff0b9e4b851b716a
Harry Schlote <harry.schlote19@imperial.ac.uk>
2019-10-29 11:21:13 +0000

    completed toys!

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -4,25 +4,25 @@ toys.py
 Simple toy functions to get really proper really comfortable working 
 with functions.
 '''
-
+print('Hello')
 
 # write a function that adds 1
 # to the input and prints the result
 def inc(a):
-    print(a)
+    print(a + 1)
 
 
 # write a function that adds 1
 # to the input and returns the result
 def inc_return(a):
-    return # hint this is incomplete
+    return a + 2
 
 
 # write a function that adds
 # the two input numbers together
 # and returns the sum
 def sum(a, b):
-    return
+    return a + b
 
 
 # write a function that takes two
@@ -30,14 +30,14 @@ def sum(a, b):
 # sum() and then increments the sum
 # using inc_return
 def sum_inc(a, b):
-    return
+    return a + b + 1
 
 
 # write a function that returns a 
 # boolean (True or False) for whether 
 # the input number is even
 def is_even(a):
-    return
+    return (a % 2) == 0
 
 
 # create for loop that takes a string
@@ -49,5 +49,5 @@ def is_even(a):
 def string_repeat(phrase, repeat):
     # hint: you can add strings together 
     # in order to concatenate them
-    return
+    return phrase * repeat
 
