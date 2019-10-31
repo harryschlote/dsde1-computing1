6d79b37c812a2fb0247204de4f5d96b710728dab
Harry Schlote <harry.schlote19@imperial.ac.uk>
2019-10-31 15:46:19 +0000

    done fuzzy

--- Week-4/fuzzy.py
+++ Week-4/fuzzy.py
@@ -6,27 +6,24 @@ Lint this file using PyLint.
 
 # This function does some maths on three numbers.
 def maths(input_a, input_b, input_c):
+    '''doc'''
     result = input_a * 3 - input_b + input_c
     return result
 
 # This function returns True or False.
 def choices(question):
-    if(question == True):
-        return True
-    else:
-        return False
-
+    '''doc'''
+    return question
 
 def main():
-    this_variable = 'useless'
-    
+    '''doc'''
     # first function takes three numbers
-    answer = maths(3, 9 , 2.3)
+    answer = maths(3, 9, 2.3)
     print(answer)
 
     # second function takes a True or False
-    newAnswer = choices(True)
-    print(newAnswer)
+    new_answer = choices(True)
+    print(new_answer)
 
 if __name__ == '__main__':
     main()
